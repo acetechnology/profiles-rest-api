d815fc8cebf374f0770377a40b70218ab55b97e2
Amaechi Osazuwa Desmond <osazuwa.amaechi@itara.co>
2020-05-14 02:43:54 +0100

    Enable django admin for user profiladded hello api view

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Returns a list of APIView features"""
+        an_apiview = [
+        'Uses HTTP mthods as function (get,post,patch,put,delete)',
+        'Is similar to a traditional Django View',
+        'Gives you the most control over your application logic',
+        'is mapped manually to URLs',
+        ]
+
+        return Response({'message':'Hello',
+        'an_apiview': an_apiview})
